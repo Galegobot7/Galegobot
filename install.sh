daad8c418fe63a5b2807ea568d92cae2b345d2e3
Galegobot7 <81787241+Galegobot7@users.noreply.github.com>
2021-04-02 08:38:04 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Galegobot
+```bash 
+> termux-setup-storage (after that tap on permission) 
+> apt install git -y 
+> git clone https://github.com/Galegobot7 
+> cd Galegobot7 
+> bash install.sh
+
+## INICIAR 
+> npm start 
+> bash
+
+### ENTREM EM CONTATO COMIGO!
+Whatsapp: wam.me/+5581996282293
+Instagram: @cafeehpaia
